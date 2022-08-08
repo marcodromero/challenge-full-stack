244799cbd90fc41a5eb6589c75d10eaa1b8f8075
Marco Romero <marcod.romero@gmail.com>
2022-08-08 04:01:44 -0300

    Instalar dependencia MySql y armar la conexión a la base de datos

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const mysql = require('mysql');
+
 const app = express();
+
+
+const PORT = process.eventNames.PORT || 8080;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'personal_budget',
+});
+
 
 app.post("/test", (req, res) => {
     console.log("Connected to React");
     res.redirect("/");
 });
 
-const PORT = process.eventNames.PORT || 8080;
+connection.connect(error =>{
+    if(error) throw error;
+    console.log('Database server running');
+});
 
 app.listen(PORT , console.log(`Server started on port ${PORT}`));
