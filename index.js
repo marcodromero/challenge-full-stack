8218f57b54fadfb1a85f2bf30e845e7286d14257
Marco Romero <marcod.romero@gmail.com>
2022-08-18 08:02:23 -0300

    Agregar verificaciones de formato y cantidad de datos recibidos en la api.

--- index.js
+++ index.js
@@ -42,51 +42,79 @@ app.get('/operations',(req, res)=>{
 
 app.post('/operations',(req, res)=>{
    const {type, concept, amount, date} = req.body;
+   let errorStatus = 0;
+   let msgStatus;
    
-   if(type != "egreso" && type != "ingreso") res.status(400).send('Tipo no valido.');
-   if(concept === '') res.status(400).send('Concepto no valido.');
-   if(amount === '') res.status(400).send('Monto no valido.');
-   if(date === '') res.status(400).send('Fecha no valida.');
-   
+   if(type != "egreso" && type != "ingreso"){msgStatus = 'Tipo no valido'; errorStatus = 1};
+   if(concept === '') {msgStatus = 'Concepto no puede estar vacio.'; errorStatus = 1};
+   if(amount === '') {msgStatus = 'Monto no puede estar vacio.'; errorStatus = 1};
+   let _amount = `${amount}`;
+   if(isNaN(_amount)) {msgStatus = 'Monto debe ser numerico.'; errorStatus = 1};
+   const expression = /^(20[2-9][0-9])(\/)(0[1-9]|1[0-2])(\/)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+   if(expression.test(date) === false){msgStatus = 'Fecha no respeta el formato.'; errorStatus = 1};
 
-   const query = `INSERT INTO operations (type, concept, amount, date) VALUES("${type}", "${concept}", ${amount}, "${date}" )`;
-  
-   connection.query(query,(error, response) =>{
-        if(error){
-            console.log(error.sqlMessage);
-            res.status(400).send('Fail');
-        }else{
-            res.status(201).send('Operation created');
-        }
-        
-    })
+   if(errorStatus == 0){
+        const query = `INSERT INTO operations (type, concept, amount, date) VALUES("${type}", "${concept}", ${amount}, "${date}" )`;
     
+        connection.query(query,(error, response) =>{
+            if(error){console.log(error.sqlMessage);}
+            else{
+                res.status(201).send('Operation created');    
+            }
+        })
+    }
+    else{
+        res.status(400).send(msgStatus);    
+        }
 })
 
 app.patch('/operations',(req, res)=>{
     const {id_operation, concept, amount, date} = req.body;
-    const query = `UPDATE operations SET concept = "${concept}", amount= ${amount}, date = "${date}" WHERE id_operation = ${id_operation}`;
-    connection.query(query,(error) =>{
-        if(error){
-            console.log(error.sqlMessage);
-            res.status(400).send('Fail');
-        }else{
-            res.status(204).send('Operation updated');
-        }
-   })
+    let errorStatus = 0;
+    let msgStatus;
+   
+    if(concept === '') {msgStatus = 'Concepto no puede estar vacio.'; errorStatus = 1};
+    if(amount === '') {msgStatus = 'Monto no puede estar vacio.'; errorStatus = 1};
+    let _amount = `${amount}`;
+    if(isNaN(_amount)) {msgStatus = 'Monto debe ser numerico.'; errorStatus = 1};
+    const expression = /^(20[2-9][0-9])(\/)(0[1-9]|1[0-2])(\/)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+    if(expression.test(date) === false){msgStatus = 'Fecha no respeta el formato.'; errorStatus = 1};
+
+    if(errorStatus == 0){
+        const query = `UPDATE operations SET concept = "${concept}", amount= ${amount}, date = "${date}" WHERE id_operation = ${id_operation}`;
+        connection.query(query,(error) =>{
+            if(error){console.log(error.sqlMessage);}
+            else{
+                res.status(204).send('Operation updated');
+            }
+        })
+    }
+    else{
+        res.status(400).send(msgStatus);    
+    }
 })
 
 app.delete('/operations',(req, res)=>{
     const {id_operation} = req.body;
-    const query = `DELETE FROM operations WHERE id_operation = ${id_operation}`;
-    connection.query(query,(error, results) =>{
-        if(error){
-            console.log(error.sqlMessage);
-            res.status(400).send('Fail');
-        }else{
-            res.status(204).send('Operation deleted');
-        }
-    })
+    let errorStatus = 0;
+    let msgStatus;
+
+    let _id_operation = `${id_operation}`;
+    if(isNaN(_id_operation)) {msgStatus = 'Id de operación no valido.'; errorStatus = 1};
+
+    if(errorStatus == 0){
+        const query = `DELETE FROM operations WHERE id_operation = ${id_operation}`;
+        connection.query(query,(error, results) =>{
+            if(error){console.log(error.sqlMessage);}
+            else{
+                res.status(204).send('Operation deleted');
+            }
+        })
+    }
+    else{
+        res.status(400).send(msgStatus);    
+    }
+
 })
 
 app.get('/balance',(req, res)=>{
